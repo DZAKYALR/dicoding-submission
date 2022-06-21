ed5a82f46808b398716d06e23d30b3996850cf5b
unknown <0dzakyalr@gmail.com>
2022-06-21 11:33:28 +0700

    dicoding finale

---
+++ src/content_option.js
@@ -0,0 +1,138 @@
+const logotext = "Dzaky";
+const meta = {
+    title: "Dzaky Al R",
+    description: "I’m Dzaky Al Ridho Fullstack developer, currently working in Jakarta",
+};
+
+const introdata = {
+    title: "I’m Dzaky",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+        third: "I develop mobile apps",
+    },
+    description: "Software Engineer with a demonstrated history of working in the information technology and services industry.",
+    your_img_url: "https://storage.googleapis.com/dicoding-submission-dzaky/profile2-removebg-preview.png",
+};
+
+const dataabout = {
+    title: "A bit about my self",
+    aboutme: "Software Engineer with a demonstrated history of working in the information technology and services industry.",
+};
+const worktimeline = [
+        {
+        jobtitle: "Fullstack Developer",
+        where: "Profesimu",
+        date: "2021",
+    },
+    {
+        jobtitle: "Software Engineer",
+        where: "ITMS",
+        date: "2020",
+    },
+];
+
+const skills = [
+    {
+        name: "Javascript",
+        value: 90,
+    },
+    {
+        name: "PERN",
+        value: 80,
+    },
+    {
+        name: "MERN",
+        value: 80,
+    },
+    {
+        name: "MEVN",
+        value: 80,
+    },
+    {
+        name: "JAM",
+        value: 80,
+    },
+    {
+        name: "Apollo",
+        value: 80,
+    },
+];
+
+const services = [{
+        title: "UI & UX Design",
+        description: "Low key simple app basic theme, and many more concept.",
+    },
+    {
+        title: "Mobile Apps",
+        description: "Create stunning app with high security.",
+    },
+    {
+        title: "Cloud Platform",
+        description: "Migrate solution for your app.",
+    },
+];
+
+const dataportfolio = [
+    {
+        img: "https://storage.googleapis.com/dicoding-submission-dzaky/315-400x700-grayscale.jpg",
+        desctiption: "Gambar diambil dari google cloud storage",
+        link: "#",
+    },
+    {
+        img: "https://storage.googleapis.com/dicoding-submission-dzaky/1019-400x400-grayscale.jpg",
+        desctiption: "Gambar diambil dari google cloud storage",
+        link: "#",
+    },
+    {
+        img: "https://storage.googleapis.com/dicoding-submission-dzaky/153-400x600-grayscale.jpg",
+        desctiption: "Gambar diambil dari google cloud storage",
+        link: "#",
+    },
+    {
+        img: "https://storage.googleapis.com/dicoding-submission-dzaky/471-400x800-grayscale.jpg",
+        desctiption: "Gambar diambil dari google cloud storage",
+        link: "#",
+    },
+    {
+        img: "https://storage.googleapis.com/dicoding-submission-dzaky/669-400x300-grayscale.jpg",
+        desctiption: "Gambar diambil dari google cloud storage",
+        link: "#",
+    },
+    {
+        img: "https://storage.googleapis.com/dicoding-submission-dzaky/933-400x400-grayscale.jpg",
+        desctiption: "Gambar diambil dari google cloud storage",
+        link: "#",
+    },
+
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "callme@dzakyalr.com",
+    YOUR_FONE: "-",
+    description: "freely to discus about your need",
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/DZAKYALR",
+    facebook: "https://facebook.com",
+    linkedin: "https://linkedin.com/in/dzakyalr",
+    twitter: "https://twitter.com",
+    youtube: "https://youtube.com",
+    twitch: "https://twitch.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
